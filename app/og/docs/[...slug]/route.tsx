7d5d58ea6c6c536240670d1ea7e349aef10f0dd7
Jerrie Pelser <jerrie@jerriepelser.com>
2025-11-12 08:14:39 +0700

    Fixes site title

--- app/og/docs/[...slug]/route.tsx
+++ app/og/docs/[...slug]/route.tsx
@@ -1,13 +1,13 @@
-import { getPageImage, source } from '@/lib/source';
-import { notFound } from 'next/navigation';
-import { ImageResponse } from 'next/og';
-import { generate as DefaultImage } from 'fumadocs-ui/og';
+import { getPageImage, source } from "@/lib/source";
+import { notFound } from "next/navigation";
+import { ImageResponse } from "next/og";
+import { generate as DefaultImage } from "fumadocs-ui/og";
 
 export const revalidate = false;
 
 export async function GET(
   _req: Request,
-  { params }: RouteContext<'/og/docs/[...slug]'>,
+  { params }: RouteContext<"/og/docs/[...slug]">
 ) {
   const { slug } = await params;
   const page = source.getPage(slug.slice(0, -1));
@@ -18,13 +18,13 @@ export async function GET(
       <DefaultImage
         title={page.data.title}
         description={page.data.description}
-        site="My App"
+        site="StellarAdmin"
       />
     ),
     {
       width: 1200,
       height: 630,
-    },
+    }
   );
 }
 
