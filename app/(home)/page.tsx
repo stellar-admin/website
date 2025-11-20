b504ee9eb271bca1f34a0b68ad9db16b8f78a87a
Jerrie Pelser <jerrie@jerriepelser.com>
2025-11-20 19:55:00 +0700

    Changes docs folder layout

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage() {
       <h1 className="text-2xl font-bold mb-4">Hello World</h1>
       <p>
         You can open{" "}
-        <Link href="/docs/stellarui" className="font-medium underline">
+        <Link href="/docs/tag-helpers" className="font-medium underline">
           /docs
         </Link>{" "}
         and see the documentation.
