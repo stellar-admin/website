18a2635d18e0b14a910d90c84fc07bff6367e698
Jerrie Pelser <jerrie@jerriepelser.com>
2025-11-29 20:13:44 +0700

    Some small fixes

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from "next";
 import Hero from "@/components/hero";
+
+export const metadata: Metadata = {
+  title: "StellarAdmin - Create beautiful ASP.NET Core apps",
+  description:
+    "StellarAdmin Tag Helpers is a collection of beautifully designed components you can use to create CRUD screens in ASP.NET Core MVC and Razor Pages applications.",
+};
 
 export default function HomePage() {
   return <Hero />;
